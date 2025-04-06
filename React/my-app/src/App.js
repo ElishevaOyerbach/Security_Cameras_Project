d04cf48b83612d6e896216a4b45ff982d5660bda
ElishevaOyerbach <=elisheva71533@gmail.com>
2025-04-06 16:12:38 +0300

    merge with node js

---
+++ React/my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './Components/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App</h1>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
